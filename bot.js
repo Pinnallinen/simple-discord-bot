ebf653e866648a7d6854d45ceb2b365048531f1d
Miikka Mänttäri <manttari.miikka@gmail.com>
2019-01-29 20:42:58 +0200

    Made !kirjoita command work

--- bot.js
+++ bot.js
@@ -1,71 +1,49 @@
 // const Discord = import("discord.io");
 // const auth = import("./auth.json");
 
-const Discord = require("discord.io");
+const Discord = require("discord.js");
 
 const auth = require("./auth.json");
 
 
+var bot = new Discord.Client();
 
-// Initializing the bot
-// var bot = new Discord.Client({
-//    token: auth.token,
-//    autorun: true
-// });
-//
-// bot.on("ready", ( event ) => {
-//     console.log("test");
-//     console.log(`Bot connected. \n Logged in as: ${bot.username} id:  ${bot.id}`);
-// });
+console.log(bot);
+console.log(bot.login(auth.token));
 
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    console.log("test");
-    console.log(bot.username + ' - (' + bot.id + ')');
+bot.on("ready", () => {
+    console.log(`Bot connected. \n`);
 });
 
+bot.on("message", ( message ) => {
+    var content = message.content;
+    if ( content.substring(0,1) === "!" ) {
+        var text = content.substring(1).split(" ");
 
-bot.on("message", ( user, userID, channelID, message, event ) => {
-    //
-    console.log("test");
-    try {
-        if ( message.substring(0, 1) === "!" ) {
-            var args = message.substring(1).split(' ');
-            var command = args[0];
-            command = command.toLowerCase();
-
+        var command = text[0];
             switch ( command ) {
                 case "auta":
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Tämänhetkiset tuetut komennot ovat: \n' +
-                                '!auta \n' +
-                                '!kirjoita jotain\\_tekstiä, korvaa välilyönnit \\_ merkillä! \n'
-                    });
+                    message.reply('Tämänhetkiset tuetut komennot ovat: \n' +
+                            '!auta \n' +
+                            '!kirjoita jotain\\_tekstiä, korvaa välilyönnit \\_ merkillä! \n');
                     break;
 
                     case "kirjoita":
         				var viesti = '';
-        				var kirjoitettava = args[1];
-        				for ( var i = 0; i < kirjoitettava.length; i++ ) {
-        					if ( kirjoitettava.charAt(i) == '_' )
+                        console.log(text.toString());
+        				var textToWrite = text[1];
+                        textToWrite = textToWrite.toLowerCase();
+                        console.log(textToWrite);
+        				for ( var i = 0; i < textToWrite.length; i++ ) {
+        					if ( textToWrite.charAt(i) == '_' )
         						viesti += '   ';
         					else
-        						viesti += ':regional_indicator_'+ kirjoitettava.charAt(i) +':';
+        						viesti += ':regional_indicator_'+ textToWrite.charAt(i) +':';
         				}
-        				bot.sendMessage({
-        					to: channelID,
-        					message: viesti
-        				});
-        				break;
 
+        				message.reply(viesti);
+
+        				break;
             }
         }
-    }
-    catch ( error ) {
-        console.log(error);
-    }
 });
